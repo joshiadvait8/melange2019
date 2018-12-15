a09a03aee7ba62cf0fe17fa0482278668e908f2e
joshiadvait8 <joshiadvait8@gmail.com>
2018-12-15 18:26:27 +0530

    ABOUT

--- js/app.js
+++ js/app.js
@@ -16,22 +16,15 @@ dbRefObject.on('child_added',snap=>{
   var eventName = snap.child("EventName").val();
   var eventcomm = snap.child("MangingCommittee").val();
   var eventTiming = snap.child("EventTiming").val();
+  var eventDate = snap.child("EventDate").val();
   var eventId = snap.child("EventID").val();
       divCard.innerHTML += ' <div class="col-lg-3 col-md-6"><div  class="hotel">'+
               '<a href="eventDetails.html?id='+eventId+'"><div class="hotel-img">'+
                 '<img src="img/hotels/1.jpg" alt="Hotel 1" class="img-fluid">'+
               '</div>'+
               '<h3>'+eventName+'</h3>'+
-              
-              '<div class="stars">'+
-              
-                 '<i class="fa fa-clock"></i>  '+ eventTiming+
-              //   <i class="fa fa-star"></i>
-              //   <i class="fa fa-star"></i>
-              //   <i class="fa fa-star"></i>
-              //   <i class="fa fa-star"></i>
-              '</div>'+
-              '<p>'+eventcomm+'</p></a>'+
+              '<p> <strong>Time: </strong>'+ eventTiming+'</p>'+
+              '<p><strong>Date: </strong>'+eventDate+'</p></a>'
             '</div></div>'
 })
 
